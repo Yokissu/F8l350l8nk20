af8dc132d056708f120848415ddfe9e4ac325c3f
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2023-11-10 08:45:42 +0000

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,9 +11,7 @@ from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
 # Connect to MongoDB
-mongo_client = MongoClient(
-    "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"
-)  # Replace with your MongoDB connection string
+mongo_client = MongoClient(os.getenv("mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority"))
 db = mongo_client["Cluster0"]
 token_collection = db["tokens"]
 user_collection = db["users"]
